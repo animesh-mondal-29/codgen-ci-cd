5321b57e58e1a9fbf5c7c1c00d470459b5afebaa
Animesh Mondal <70890111+animesh-mondal-29@users.noreply.github.com>
2025-08-07 13:34:35 +0530

    Update get_repo.sh

--- get_repo.sh
+++ get_repo.sh
@@ -22,20 +22,14 @@ if [[ "${CI_BUILD}" != "no" ]]; then
   git config --global --add safe.directory "/__w/$( echo "${GITHUB_REPOSITORY}" | awk '{print tolower($0)}' )"
 fi
 
-VOID_BRANCH="fix-build"
+VOID_BRANCH="main"
 echo "Cloning void ${VOID_BRANCH}..."
 
 mkdir -p vscode
 cd vscode || { echo "'vscode' dir not found"; exit 1; }
 
-#git config --global url."https://andai-org:${GH_RO_PAT}@github.com/".insteadOf "https://github.com/"
-
-# git init -q
-git remote add origin https://github.com/andai-org/andai-editor.git
-git config --global user.name "animesh-mondal-29"
-git config --global user.email "anibppimt@gmail.com"
-# git config --global http.https://github.com/.extraheader "AUTHORIZATION: basic $(echo -n :$GH_RO_PAT | base64)"
-git clone https://github.com/animesh-mondal-29/codgen.git .
+git init -q
+git remote add origin https://github.com/animesh-mondal-29/codgen.git 
 
 # Allow callers to specify a particular commit to checkout via the
 # environment variable VOID_COMMIT.  We still default to the tip of the
